819c8dab4e867787b4d2b5f00ef6254272490267
Facundo Arca <farca9@gmail.com>
2018-11-06 20:55:39 -0300

    configuracion del mapafragment

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MainActivity.java
@@ -60,11 +60,18 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
                                 break;
                             case R.id.optVerMapa:
                                 //TODO HABILITAR
-                                //tag="mapaReclamos";
-                               // fragment =  getSupportFragmentManager().findFragmentByTag(tag);
+                                tag="mapaReclamos";
+                                fragment =  getSupportFragmentManager().findFragmentByTag(tag);
                                 //TODO si "fragment" es null entonces crear el fragmento mapa, agregar un bundel con el parametro tipo_mapa
                                 // configurar a la actividad como listener de los eventos del mapa ((MapaFragment) fragment).setListener(this);
-                               // fragmentTransaction = true;
+                                if(fragment == null){
+                                    MapaFragment mapaFragment = new MapaFragment();
+                                    Bundle bundle = new Bundle();
+                                    bundle.putInt("tipo_mapa",0);
+                                    mapaFragment.setArguments(bundle);
+
+                                }
+                                fragmentTransaction = true;
                                 break;
                             case R.id.optHeatMap:
                                 //TODO HABILITAR
